10b8f06a5467438afcca35f3bf116a6ac1e01087
PanthaNIS <panthanischal12@gmail.com>
2022-08-15 09:23:24 +0800

    final touch

--- IonicAppNative/src/app/app.module.ts
+++ IonicAppNative/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { Device } from '@awesome-cordova-plugins/device/ngx';
 import { TextToSpeechAdvanced } from '@awesome-cordova-plugins/text-to-speech-advanced/ngx';
 import { NativeAudio } from '@awesome-cordova-plugins/native-audio/ngx';
 import { DeviceOrientation } from '@awesome-cordova-plugins/device-orientation/ngx';
+import { BatteryStatus } from '@awesome-cordova-plugins/battery-status/ngx';
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
 
 
 
@@ -19,7 +22,7 @@ import { DeviceOrientation } from '@awesome-cordova-plugins/device-orientation/n
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
-    Device,TextToSpeechAdvanced,NativeAudio,DeviceOrientation,
+    Device,TextToSpeechAdvanced,NativeAudio,DeviceOrientation,BatteryStatus, Flashlight, SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- IonicAppNative/src/app/tab5/tab5.page.ts
+++ IonicAppNative/src/app/tab5/tab5.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { BatteryStatus } from '@awesome-cordova-plugins/battery-status/ngx';
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
-export class Tab5Page implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class Tab5Page {
+  subscription : any;
+  percentage: number;
+  isPlugged: boolean;
+  message: string;
+  constructor(private batteryStatus: BatteryStatus, private flashlight: Flashlight) {
+    // reference: https://www.positronx.io/ionic-check-battery-status-and-charging-state/
+      //check the battery status
+    this.subscription = this.batteryStatus.onChange().subscribe(status => {
+      this.percentage = status.level;
+      this.isPlugged = status.isPlugged;
+      alert("Level: " + status.level + " Is plugged: " + status.isPlugged);
+    });
   }
 
-}
+  // toggle the flashlight function
+  toggleFlashlight() {
+    if (this.percentage > 50){
+      this.flashlight.toggle();
+      this.message = "You can turn the flashlight on";
+    } else {
+      this.message = "The battery percentage is low, please charge the device and you can't turn the flashlight on now.";
+    }
+  }
+
+   }
+
+
+
+ 
+
+  
